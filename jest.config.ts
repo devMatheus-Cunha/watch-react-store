e19768e22f0874a3c248d9589bd4dd3d8bcdf6f2
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-10-18 22:06:02 +0100

    refactor: new config jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,15 @@
-export const testEnvironment = "jsdom";
-export const testPathIgnorePatterns = ["/node_modules/", "/.next/"];
-export const collectCoverage = true;
-export const collectCoverageFrom = ["<rootDir>/components/**/**/*.ts(x)", "<rootDir>/pages/*.ts(x)", "<rootDir>/hooks/**/**/*.ts(x)"];
-export const setupFilesAfterEnv = ["<rootDir>/.jest/setup.ts"];
+import type { Config } from 'jest';
+import { defaults } from 'jest-config';
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "<rootDir>/components/**/**/*.ts(x)",
+    "<rootDir>/pages/*.ts(x)"
+  ],
+  setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
+};
+
+export default config;

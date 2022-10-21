0d5f77a2ef8fd3e352b33dda98000d989c1e40df
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-10-21 12:25:24 +0100

    feat: added test should decrease quantity by 1 when first button is clicked

--- components/CartItem/test.spec.tsx
+++ components/CartItem/test.spec.tsx
@@ -45,14 +45,25 @@ describe('CartItem', () => {
  it('should increase quantity by 1 when second button is clicked', async () => {
   renderCartItem()
 
-  const [button, _] = screen.getAllByRole('button')
+  const [_, button] = screen.getAllByRole('button')
 
   await fireEvent.click(button)
 
   expect(screen.getByTestId('quantity').textContent).toBe('2')
  });
 
- it.todo('should decrease quantity by 1 when first button is clicked');
+ it('should decrease quantity by 1 when first button is clicked', async () => {
+  renderCartItem()
+  const quantity = screen.getByTestId('quantity')
+
+  const [buttonDecrese, buttonIncrease] = screen.getAllByRole('button')
+
+  await fireEvent.click(buttonIncrease)
+  expect(quantity.textContent).toBe('2')
+
+  await fireEvent.click(buttonDecrese)
+  expect(quantity.textContent).toBe('1')
+ });
 
  it.todo('should not go below zero uin the quantity');
 })

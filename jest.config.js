c4b5138f84d2e180c6f6b113dc7e16a664e4aca6
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-10-24 11:22:30 +0100

    feat: added command to test only integration tests and unit tests

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  collectCoverage: false,
+  collectCoverageFrom: ["<rootDir>/components/**/**/*.ts(x)", "<rootDir>/pages/*.ts(x)", "<rootDir>/hooks/**/**/*.ts(x)"],
+  setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
+};

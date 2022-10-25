322e304854a509b195830496ee988f4d94cb2b66
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-10-25 12:04:24 +0100

    feat: add test to page index should render the ProductCard component 10 times

--- pages-tests/product-list.integration.spec.tsx
+++ pages-tests/product-list.integration.spec.tsx
@@ -2,13 +2,41 @@ import {
  render,
  screen,
  fireEvent,
+ waitFor,
 } from '@testing-library/react'
 import ProductList from '../pages/index'
+import { makeServer, TServer } from '../services/mirage/server'
+
+const renderProductList = () => {
+ render(<ProductList />)
+}
 
 describe('ProductList', () => {
+ let server: TServer
+
+ beforeEach(() => {
+  server = makeServer({ environment: 'test' })
+ })
+
+ afterEach(() => {
+  server.shutdown()
+ })
+
  it('should render page ProductList', () => {
-  render(<ProductList />)
+  renderProductList()
 
   expect(screen.getByTestId('product-list')).toBeInTheDocument()
+ });
+
+ it('should render the ProductCard component 10 times', async () => {
+  server.createList('product', 10)
+
+
+  renderProductList()
+
+  await waitFor(() => {
+   expect(screen.getAllByTestId('product-card')).toHaveLength(10)
+  })
+
  });
 })

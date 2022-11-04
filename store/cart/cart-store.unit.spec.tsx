0aaf617871aa8c98851d7066ffdf11dada9810d7
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-11-04 11:13:34 +0000

    refactor: structure test cart-store

--- store/cart/cart-store.unit.spec.tsx
+++ store/cart/cart-store.unit.spec.tsx
@@ -4,33 +4,33 @@ import { makeServer, TServer } from '../../services/mirage/server'
 
 describe('Cart Store', () => {
   let server: TServer
+  let result: any
+  let add: any
+  let toggle: () => void;
 
   beforeEach(() => {
     server = makeServer({ environment: 'test' })
+    result = renderHook(() => useCartStore()).result
+    add = result.current.actions.add;
+    toggle = result.current.actions.toggle
   })
 
   afterEach(() => {
     server.shutdown()
+    act(() => result.current.actions.reset())
   })
 
   it('should return open equals false on initial state', async () => {
-    const { result } = renderHook(() => useCartStore())
-
     expect(result.current.state.open).toBe(false)
   });
 
   it('should return an empty a array for products on initial state', async () => {
-    const { result } = renderHook(() => useCartStore())
-
     expect(Array.isArray(result.current.state.products)).toBe(true)
     expect(result.current.state.products).toHaveLength(0)
   });
 
   it('should increase products when addProduct functions is called', async () => {
     const products = server.createList('product', 2)
-
-    const { result } = renderHook(() => useCartStore())
-    const { actions: { add } } = result.current
 
     for (const product of products) {
       act(() => add(product))
@@ -40,9 +40,6 @@ describe('Cart Store', () => {
   });
 
   it('should toggle open state', async () => {
-    const { result } = renderHook(() => useCartStore())
-    const { actions: { toggle } } = result.current
-
     expect(result.current.state.open).toBe(false)
 
     act(() => toggle())

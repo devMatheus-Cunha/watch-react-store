1c23171a1fc5690d9115975c6a07194fe600bf0b
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-11-04 11:03:50 +0000

    feat: added test should return an empty a array for products on initial state

--- store/cart/cart-store.unit.spec.tsx
+++ store/cart/cart-store.unit.spec.tsx
@@ -1,11 +1,29 @@
 import { renderHook, act } from "@testing-library/react-hooks"
 import { useCartStore } from "../../store/cart"
+import { makeServer, TServer } from '../../services/mirage/server'
 
 describe('Cart Store', () => {
+  let server: TServer
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' })
+  })
+
+  afterEach(() => {
+    server.shutdown()
+  })
+
   it('should return open equals false on initial state', async () => {
     const { result } = renderHook(() => useCartStore())
 
     expect(result.current.state.open).toBe(false)
+  });
+
+  it('should return an empty a array for products on initial state', async () => {
+    const { result } = renderHook(() => useCartStore())
+
+    expect(Array.isArray(result.current.state.products)).toBe(true)
+    expect(result.current.state.products).toHaveLength(0)
   });
 
   it('should toggle open state', async () => {

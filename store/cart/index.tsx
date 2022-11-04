d16bfa43b55b34526d180d59af1bb627a836c8ea
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-11-04 11:13:54 +0000

    feat: added mehot reset, add and initialState in useCartStore

--- store/cart/index.tsx
+++ store/cart/index.tsx
@@ -1,19 +1,39 @@
 import create from 'zustand'
 
 interface IUseCartStore {
- state: {
-  open: boolean
- }
- actions: {
-  toggle: () => void
- }
+  state: {
+    open: boolean,
+    products: any[]
+  }
+  actions: {
+    toggle: () => void
+    add: (data: any) => void
+    reset: () => void
+  }
+}
+
+const initialState = {
+  open: false,
+  products: [],
 }
 
 export const useCartStore = create<IUseCartStore>((set) => ({
- state: {
-  open: false
- },
- actions: {
-  toggle: () => set((store: any) => ({ state: { open: !store.state.open } })),
- },
+  state: initialState,
+  actions: {
+    toggle: () =>
+      set((store) => ({
+        state: { ...store.state, open: !store.state.open },
+      })),
+    reset: () =>
+      set(() => ({
+        state: { ...initialState },
+      })),
+    add: (product) =>
+      set((store) => ({
+        state: {
+          open: true,
+          products: [...store.state.products, product]
+        }
+      })),
+  },
 }))

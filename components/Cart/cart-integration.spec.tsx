cbffac3d869874321b8f65e7d567a988b9f2a0be
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-11-07 11:46:45 +0000

    feat: added test should render component

---
+++ components/Cart/cart-integration.spec.tsx
@@ -0,0 +1,35 @@
+import { renderHook, act } from "@testing-library/react-hooks"
+import { render, screen } from "@testing-library/react";
+import { useCartStore } from "../../store/cart"
+import { makeServer, TServer } from '../../services/mirage/server'
+import Cart from ".";
+
+describe('Cart', () => {
+ let server: TServer
+ let result: any
+ let spy: any
+ let add: any
+ let toggle: any
+ let reset: any
+
+ beforeEach(() => {
+  server = makeServer({ environment: 'test' })
+  result = renderHook(() => useCartStore()).result
+  add = result.current.actions.add;
+  reset = result.current.actions.reset;
+  toggle = result.current.actions.toggle
+  spy = jest.spyOn(result.current.actions, 'toggle')
+ })
+
+ afterEach(() => {
+  server.shutdown()
+  jest.clearAllMocks()
+ })
+
+ it('should render component', () => {
+  render(<Cart />)
+
+  expect(screen.getByTestId('cart')).toBeInTheDocument()
+ });
+
+});

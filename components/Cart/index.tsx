cefa4b40f230cbb093b2756d3424a23268e9b02f
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-11-09 11:15:13 +0000

    refactor: component Cart

--- components/Cart/index.tsx
+++ components/Cart/index.tsx
@@ -11,6 +11,8 @@ const Cart: React.FC = () => {
   const { open, products } = useCartStore((store) => store.state)
   const toggle = useCartStore((store) => store.actions.toggle)
 
+  const hasProducts = products.length > 0
+
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
@@ -40,22 +42,17 @@ const Cart: React.FC = () => {
         </button>
       </div>
       <hr className="my-3" />
-      {products.map((product) => <CartItem product={product} key={product.id} />)}
-
-      <div className="mt-8">
-        <form className="flex items-center justify-center">
-          <input
-            className="form-input w-48"
-            type="text"
-            placeholder="Add promocode"
-          />
-          <button className="ml-3 flex items-center px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-            <span>Apply</span>
-          </button>
-        </form>
-      </div>
-      <a className="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
-        <span>Chechout</span>
+      {
+        hasProducts ? (
+          <>
+            {products.map((product) => <CartItem product={product} key={product.id} />)}
+          </>
+        ) : (
+          <h3 className="text-center font-bold text-blue-600">There are no items i the cart</h3>
+        )
+      }
+      <a className={`${!hasProducts ? "hidden" : ""} flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500"`}>
+        <span > Checkout</span>
         <svg
           className="h-5 w-5 mx-2"
           fill="none"
@@ -68,7 +65,7 @@ const Cart: React.FC = () => {
           <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
         </svg>
       </a>
-    </div>
+    </div >
   );
 };
 

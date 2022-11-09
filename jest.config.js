43fda102aa5b905d3e7f235f9791d629e5c3e384
devMatheus-Cunha <devmatheusgr@gmail.com>
2022-11-09 11:38:26 +0000

    feat: added file store in jest.config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   testEnvironment: "jsdom",
   testPathIgnorePatterns: ["/node_modules/", "/.next/"],
   collectCoverage: false,
-  collectCoverageFrom: ["<rootDir>/components/**/**/*.ts(x)", "<rootDir>/pages/*.ts(x)", "<rootDir>/hooks/**/**/*.ts(x)"],
+  collectCoverageFrom: [
+    "<rootDir>/components/**/**/*.ts(x)",
+    "<rootDir>/pages/*.ts(x)",
+    "<rootDir>/hooks/**/**/*.ts(x)",
+    "<rootDir>/store/**/**/*.ts(x)",
+  ],
   setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
 };
